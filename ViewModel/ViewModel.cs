53837a9999660b94297ea7e96f8aeda4960acd51
ChrisKolan <chris.kolan@gmail.com>
2019-05-11 22:54:11 +0200

    Implemented YoutubeDlUpdater

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -1,22 +1,25 @@
-﻿using System;
+﻿using Octokit;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Voltsoft.FileVersionUtilities;
 
 namespace ViewModel
 {
     public class ViewModel : INotifyPropertyChanged
     {
         private string _selectedQuality;
-        //private string _standardOutput;
 
         public ViewModel()
         {
             Model = new Model.Model();
+            var updaterTask = YoutubeDlUpdater();
             Quality = new ObservableCollection<string>
             {
                 "Audio quality: 0 Best (Largest mp3 file size)",
@@ -48,15 +51,36 @@ namespace ViewModel
             }
         }
 
-        //public string StandardOutput
-        //{
-        //    get { return _standardOutput; }
-        //    set
-        //    {
-        //        _standardOutput = Model.CurrentStandardOutputLine;
-        //        OnPropertyChanged(nameof(StandardOutput));
-        //    }
-        //}
+        private async Task YoutubeDlUpdater()
+        {
+            Model.StandardOutput = "Checking if new version is available.";
+            var pathToExeFolder = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string pathToYoutubeDl = pathToExeFolder + @"\bin\youtube-dl.exe";
+            var currentYoutubeDlVersion = Utilities.GetFileVersion(pathToYoutubeDl);
+
+            var client = new GitHubClient(new ProductHeaderValue("youtube-dl"));
+            client.SetRequestTimeout(TimeSpan.FromSeconds(20));
+            var releases = await client.Repository.Release.GetLatest("ytdl-org", "youtube-dl");
+            var newYoutubeDlVersion = releases.TagName;
+
+            if (currentYoutubeDlVersion != newYoutubeDlVersion)
+            {
+                Model.StandardOutput = "Downloading new Youtube-dl version. Current version: " + currentYoutubeDlVersion + ". New version: " + newYoutubeDlVersion;
+                Model.IsIndeterminate = true;
+                var latestAsset = await client.Repository.Release.GetAllAssets("ytdl-org", "youtube-dl", releases.Id);
+                var latestUri = latestAsset[7].BrowserDownloadUrl;
+
+                var response = await client.Connection.Get<object>(new Uri(latestUri), new Dictionary<string, string>(), "application/octet-stream");
+                var responseData = response.HttpResponse.Body;
+                System.IO.File.WriteAllBytes(pathToYoutubeDl, (byte[])responseData);
+                Model.StandardOutput = "Updated Youtube-dl to version: " + newYoutubeDlVersion + ". Status: idle";
+            }
+            else
+            {
+                Model.StandardOutput = "Status: idle";
+            }
+            Model.IsIndeterminate = false;
+        }
 
         private void DownloadButtonCommand()
         {
