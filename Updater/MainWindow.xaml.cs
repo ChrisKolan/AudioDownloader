de3fbdf2bb10d8bba0beb028e88093622a4f86f3
ChrisKolan <chris.kolann@gmail.com>
2019-08-26 22:47:56 +0200

    Added ViewModel

--- Updater/MainWindow.xaml.cs
+++ Updater/MainWindow.xaml.cs
@@ -24,71 +24,11 @@ namespace Updater
     /// </summary>
     public partial class MainWindow
     {
-        private static string _pathToExe = Assembly.GetEntryAssembly().Location;
-        private static string _pathToExeFolder = System.IO.Path.GetDirectoryName(_pathToExe);
-        //private TextBlock _TextBlock = new TextBlock();
-
         public MainWindow()
         {
             InitializeComponent();
-
-            
-            //_TextBlock.TextWrapping = TextWrapping.Wrap;
-            //_TextBlock.Margin = new Thickness(10);
-            //_TextBlock.Inlines.Add("Updating AudioDownloader...");
-            //Content = _TextBlock;
-
-            //try
-            //{
-            //    StopAudioDownloader();
-            //    DeleteOldFiles();
-            //    StartAudioDownloader();
-            //}
-            //catch (Exception exception)
-            //{
-            //    Console.WriteLine("\n\nApplication in an inconsistent state. Please download the latest version manually from:\nhttps://chriskolan.github.io/audio-downloader/\n\n");
-            //    StopAudioDownloader();
-            //    Console.WriteLine("\n\nPlease consider opening an issue and pasting the exception description at:\nhttps://github.com/ChrisKolan/audio-downloader/issues");
-            //    Console.WriteLine("\nUpdater exception: " + exception);
-            //    Console.WriteLine("\n\nPress any key to close this window.");
-            //    Console.ReadKey();
-            //}
-            //_TextBlock.Inlines.Add("\nUpdated!");
-        }
-
-        private void StopAudioDownloader()
-        {
-            //_TextBlock.Inlines.Add("\nClosing Audio Downloader.");
-            foreach (var process in Process.GetProcessesByName("AudioDownloader"))
-            {
-                process.Kill();
-            }
-            Thread.Sleep(100);
-        }
-
-        private void DeleteOldFiles()
-        {
-            //_TextBlock.Inlines.Add("\nDeleting old files.");
-            DirectoryInfo directoryInfo = new DirectoryInfo(_pathToExeFolder);
-            FileInfo[] fileInfoArray = directoryInfo.GetFiles("old_*.*");
-            foreach (FileInfo file in fileInfoArray)
-            {
-                try
-                {
-                    file.Delete();
-                }
-                catch (System.UnauthorizedAccessException)
-                {
-                    continue;
-                }
-            }
-            Thread.Sleep(100);
-        }
-        private void StartAudioDownloader()
-        {
-            //_TextBlock.Inlines.Add("\nStarting Audio Downloader.");
-            var pathToAudioDownloader = _pathToExeFolder + @"\AudioDownloader.exe";
-            Process.Start(pathToAudioDownloader);
+            var viewModel = new ViewModel();
+            DataContext = viewModel;
         }
     }
 }
---
+++ Updater/ViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Updater
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private string _standardOutput;
+        private static string _pathToExe = Assembly.GetEntryAssembly().Location;
+        private static string _pathToExeFolder = System.IO.Path.GetDirectoryName(_pathToExe);
+
+        public ViewModel()
+        {
+            StandardOutput = "Test";
+        }
+
+        public string StandardOutput
+        {
+            get { return _standardOutput; }
+            set
+            {
+                _standardOutput = value;
+                OnPropertyChanged(nameof(StandardOutput));
+            }
+        }
+
+        private void StopAudioDownloader()
+        {
+            //_TextBlock.Inlines.Add("\nClosing Audio Downloader.");
+            foreach (var process in Process.GetProcessesByName("AudioDownloader"))
+            {
+                process.Kill();
+            }
+            Thread.Sleep(100);
+        }
+
+        private void DeleteOldFiles()
+        {
+            //_TextBlock.Inlines.Add("\nDeleting old files.");
+            DirectoryInfo directoryInfo = new DirectoryInfo(_pathToExeFolder);
+            FileInfo[] fileInfoArray = directoryInfo.GetFiles("old_*.*");
+            foreach (FileInfo file in fileInfoArray)
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (System.UnauthorizedAccessException)
+                {
+                    continue;
+                }
+            }
+            Thread.Sleep(100);
+        }
+        private void StartAudioDownloader()
+        {
+            //_TextBlock.Inlines.Add("\nStarting Audio Downloader.");
+            var pathToAudioDownloader = _pathToExeFolder + @"\AudioDownloader.exe";
+            Process.Start(pathToAudioDownloader);
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
