59eff2aef6300a9d5fe7d7039884d3da3ebb7240
ChrisKolan <chris.kolann@gmail.com>
2019-08-29 00:10:50 +0200

    Update if 10 minutes (or more) has elapsed between application launches

--- Model/Updater/ApplicationUpdater.cs
+++ Model/Updater/ApplicationUpdater.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +11,81 @@ namespace Model
     {
         public static async Task UpdateAsync(Model model)
         {
-            model.StandardOutput = "Status: checking for updates.";
-            model.DisableInteractions();
+            if (UpdatesNeeded())
+            {
+                model.StandardOutput = "Status: checking for updates.";
+                model.DisableInteractions();
 
-            try
-            {
-                await UpdatesDownloader.DownloadUpdatesAsync(model);
-            }
-            catch (Exception exception)
-            {
-                model.StandardOutput = "Status: idle. Failed to update Audio Downloader.\n" + exception.ToString();
-                model.GetLocalVersions();
-                model.EnableInteractions();
-                return;
+                try
+                {
+                    await UpdatesDownloader.DownloadUpdatesAsync(model);
+                }
+                catch (Exception exception)
+                {
+                    model.StandardOutput = "Status: idle. Failed to update Audio Downloader.\n" + exception.ToString();
+                    model.GetLocalVersions();
+                    model.EnableInteractions();
+                    return;
+                }
             }
 
             model.StandardOutput = "Status: idle.";
             model.GetLocalVersions();
             model.EnableInteractions();
+        }
+
+        private static bool TryAddOrUpdateApplicationSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+
+                return true;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return false;
+            }
+        }
+        private static bool UpdatesNeeded()
+        {
+            double updateIfElapsedTimeExceeded = 10; 
+            DateTime lastStoredUpdateDateTimeFromBinary;
+            string lastStoredUpdateDateTime = ConfigurationManager.AppSettings["UpdateDateTime"];
+            if (lastStoredUpdateDateTime != null)
+            {
+                var lastStoredUpdateDateTimeLong = long.Parse(lastStoredUpdateDateTime);
+                lastStoredUpdateDateTimeFromBinary = DateTime.FromBinary(lastStoredUpdateDateTimeLong);
+                var currentUpdateDateTime = UpdateSettings();
+                var elapsedTimeBetweenUpdates = currentUpdateDateTime.Subtract(lastStoredUpdateDateTimeFromBinary).TotalMinutes;
+
+                return elapsedTimeBetweenUpdates > updateIfElapsedTimeExceeded;
+            }
+            else
+            {
+                UpdateSettings();
+
+                return true;
+            }
+        }
+        private static DateTime UpdateSettings()
+        {
+            var currentUpdateDateTime = DateTime.Now;
+            var currentUpdateDateTimeString = currentUpdateDateTime.ToBinary().ToString();
+            TryAddOrUpdateApplicationSettings("UpdateDateTime", currentUpdateDateTimeString);
+
+            return currentUpdateDateTime;
         }
     }
 }
