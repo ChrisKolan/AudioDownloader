0d4f17358347193ee46a024c974630536840e9aa
ChrisKolan <chris.kolann@gmail.com>
2019-10-26 15:17:43 +0200

    Correct AssemblyFileVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.4.1910")]
-[assembly: AssemblyFileVersion("1.0.4.1910")]
+[assembly: AssemblyFileVersion("1.9.4.1910")]
