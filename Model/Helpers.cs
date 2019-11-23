33825820563bd0927f7268f5952181f6ed6cf9bb
ChrisKolan <chris.kolann@gmail.com>
2019-11-23 16:22:45 +0100

    Best audio chosen dynamically

--- Model/Helpers.cs
+++ Model/Helpers.cs
@@ -11,7 +11,7 @@ namespace Model
         public static (string command, string finishedMessage) CreateCommandAndMessage(string selectedQuality, string date, string downloadLink)
         {
             string command, finishedMessage;
-            var youTubeBeginCommand = "/C bin\\youtube-dl.exe --format ";
+            var youTubeBeginCommand = "/C bin\\youtube-dl.exe ";
             var escapedDownloadLink = "\"" + downloadLink + "\"";
 
             if (selectedQuality.Contains("mp3"))
@@ -20,39 +20,39 @@ namespace Model
                 // https://www.youtube.com/watch?v=ojdKM43cIxQ&loop=0 does not have webm format- need to change bestaudio[ext=webm] to bestaudio[ext=m4a] dynamically
                 // https://www.youtube.com/watch?v=Nxs_mpWt2BA&list=PLczZk1L30r_s_9woWc1ZvhUNA2n_wjICI&index=2&t=0s playlista
                 //command = "/C bin\\youtube-dl.exe -f bestaudio[ext=webm] --extract-audio --audio-format mp3 --no-mtime --add-metadata --audio-quality " + quality + " --restrict-filenames -o audio\\" + date + "Q" + quality + "-%(title)s-%(id)s.%(ext)s " + escapedDownloadLink;
-                command = youTubeBeginCommand + "bestaudio[ext=webm] --extract-audio --audio-format mp3" + GenerateConstantCommand(date, escapedDownloadLink, quality);
+                command = youTubeBeginCommand + "--extract-audio --audio-format mp3" + GenerateConstantCommand(date, escapedDownloadLink, quality);
                 finishedMessage = "Download finished. Now transcoding to mp3. This may take a while. Processing.";
             }
             else if (selectedQuality.Contains("flac"))
             {
-                command = youTubeBeginCommand + "bestaudio[ext=webm] --extract-audio --audio-format flac" + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--extract-audio --audio-format flac" + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished. Now transcoding to FLAC. This may take a while. Processing.";
             }
             else if (selectedQuality.Contains("raw webm"))
             {
-                command = youTubeBeginCommand + "bestaudio[ext=webm]" + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--format bestaudio[ext=webm]" + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished";
             }
             else if (selectedQuality.Contains("raw opus"))
             {
-                command = youTubeBeginCommand + "bestaudio[acodec=opus] --extract-audio" + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--format bestaudio[acodec=opus] --extract-audio" + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished";
             }
             else if (selectedQuality.Contains("raw aac"))
             {
-                command = youTubeBeginCommand + "bestaudio[ext=m4a]" + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--format bestaudio[ext=m4a]" + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished";
             }
             else if (selectedQuality.Split(' ').First().All(char.IsDigit))
             {
                 var formatCode = selectedQuality.Split(' ').First();
                 var format = selectedQuality.Split(' ').Last();
-                command = youTubeBeginCommand + formatCode + "--extract-audio  --audio-format " + format + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--format " + formatCode + " --extract-audio  --audio-format " + format + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished";
             }
             else
             {
-                command = youTubeBeginCommand + "bestaudio[acodec=vorbis] --extract-audio" + GenerateConstantCommand(date, escapedDownloadLink, null);
+                command = youTubeBeginCommand + "--format bestaudio[acodec=vorbis] --extract-audio" + GenerateConstantCommand(date, escapedDownloadLink, null);
                 finishedMessage = "Download finished";
             }
 
