1a6dc59c403e8e053e3fd844449707d76e924472
ChrisKolan <chris.kolann@gmail.com>
2020-03-22 20:40:38 +0100

    await UpdatesDownloader.DownloadUpdatesAsync(model).ConfigureAwait(false);

--- Model/Updater/ApplicationUpdater.cs
+++ Model/Updater/ApplicationUpdater.cs
@@ -22,7 +22,7 @@ namespace Model
 
                 try
                 {
-                    await UpdatesDownloader.DownloadUpdatesAsync(model);
+                    await UpdatesDownloader.DownloadUpdatesAsync(model).ConfigureAwait(false);
                 }
                 catch (Exception exception)
                 {
