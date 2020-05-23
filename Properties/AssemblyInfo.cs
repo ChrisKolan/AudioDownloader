aea634d96431776521b2cdbfa107287848738fb9
ChrisKolan <chris.kolann@gmail.com>
2020-05-23 16:55:45 +0200

    AssemblyVersion("1.21.9.2005")

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.20.9.2005")]
-[assembly: AssemblyFileVersion("1.20.9.2005")]
+[assembly: AssemblyVersion("1.21.9.2005")]
+[assembly: AssemblyFileVersion("1.21.9.2005")]
 [assembly: NeutralResourcesLanguage("en")]
