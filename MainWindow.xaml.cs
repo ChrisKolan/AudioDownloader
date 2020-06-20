15d2eed7ae658794a55f02d5e9965e323e4cd958
ChrisKolan <chris.kolann@gmail.com>
2020-06-20 21:35:34 +0200

    Testing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ namespace Main
     {
         public MainWindow()
         {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("de-DE");
             InitializeComponent();
 
             var viewModel = new ViewModel.ViewModelClass();
